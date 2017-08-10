5565fa046a8cb2fa2055de52036a1c8661f7bfed
lpengfei <lpengfei66@163.com>
2017-08-10 14:01:05 +0800

    基本框架搭搭建

---
+++ src/components/Tab.vue
@@ -0,0 +1,31 @@
+<template>
+	<tabbar>
+      <tabbar-item>
+        <img slot="icon" src=" ">
+        <span slot="label">首页</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src=" ">
+        <span slot="label">借款</span>
+      </tabbar-item>
+      <tabbar-item selected link=" ">
+        <img slot="icon" src=" ">
+        <span slot="label">资讯</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src=" ">
+        <span slot="label">我的</span>
+      </tabbar-item>
+    </tabbar>
+</template>
+<script>
+import { Tabbar, TabbarItem } from 'vux'
+
+export default {
+  name:'Tab',
+  components: {
+    Tabbar,
+    TabbarItem
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import index from '../views/home/index.vue'
+import item from '../views/item/item.vue'
+import news from '../views/news/news.vue'
+import mine from '../views/mine/mine.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+
+const routes = [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+    	path:'/',
+    	name:'index',
+    	component:index
+    },
+    {
+    	path:'/item',
+    	name:'item',
+    	component:item
+    },
+    {
+    	path:'/news',
+    	name:'news',
+    	component:news
+    },
+    {
+    	path:'/mine',
+    	name:'mine',
+    	component:mine
     }
   ]
+
+const router = new Router({
+	routes,
+	mode:'history'
 })
+export default router;
